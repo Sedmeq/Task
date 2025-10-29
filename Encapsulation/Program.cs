74dd4ddfa57c6a73e6c873fec542248f372f0a85
Sedmeq <Sedmeq@users.noreply.github.com>
2025-10-29 11:37:09 +0400

    Encapsulation added

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,74 @@
+﻿using Encapsulation;
+
+User[] users = new User[3];
+for (int i = 0; i < users.Length; i++)
+{
+    Console.WriteLine("Fullname daxil edin:");
+    string fullname = Console.ReadLine();
+    Console.WriteLine("Email daxil edin:");
+    string email = Console.ReadLine();
+    while (true)
+    {
+        Console.WriteLine("Password daxil edin:");
+        string password = Console.ReadLine();
+        User user = new User(email, password);
+        if (user.PasswordChecker(password))
+        {
+            user.Fullname = fullname; users[i] = user;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Password sertlerini odemir, yeniden cehd edin.");
+        }
+    }
+
+
+
+}
+
+bool isFound = true;
+while (isFound)
+{
+    Console.WriteLine("1. Show all users");
+    Console.WriteLine("2. Get info by id");
+    Console.WriteLine("0. Quit");
+    int choice = Convert.ToInt32(Console.ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            foreach (var user in users)
+            {
+                user.GetInfo();
+            }
+            break;
+
+        case 2:
+            Console.WriteLine("Id daxil edin:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            bool found = false;
+            foreach (var user in users)
+            {
+                if (user.Id == id)
+                {
+                    user.GetInfo();
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                Console.WriteLine("User tapilmadi.");
+            }
+            break;
+
+        case 0:
+            isFound = false;
+            break;
+
+        default:
+            Console.WriteLine("Yanlis secim.");
+            break;
+    }
+}
---
+++ Encapsulation/User.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace Encapsulation
+{
+    internal class User
+    {
+        private static int _idCounter = 0;
+        public int Id { get; }
+        public string Fullname { get; set; }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Email bos ola bilmez!");
+                }
+                else
+                {
+                    _email = value;
+                }
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (PasswordChecker(value))
+                {
+                    _password = value;
+                }
+                else
+                {
+                    Console.WriteLine("Password sertlerini odemir! Yeniden daxil edin:");
+                }
+            }
+        }
+
+        public int Age { get; set; }
+
+        public User(string email, string password)
+        {
+            Id = _idCounter++;
+            
+            Email = email;
+            Password = password;
+            
+        }
+
+        public bool PasswordChecker(string password)
+        {
+            return password.Length >= 8 &&
+                   password.Any(char.IsDigit) &&
+                   password.Any(char.IsUpper) &&
+                   password.Any(char.IsLower);
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Id: {Id}, Fullname: {Fullname}, Email: {Email}");
+        }
+    }
+}
